90f5d500f8c5b511ae632c2ccdd0ad187afd35f3
mehmetonemli <81512836+mehmetonemli@users.noreply.github.com>
2021-03-30 13:10:09 +0300

    ilk gönderme

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine(" Musteriler ekleniyor.. ");
+            Console.WriteLine(" Musterİ Id : " + customer.Id + " Musteri adi : " + customer.Name + " Musteri Soyadi : " + customer.Surname);
+            Console.WriteLine("-----------------------");
+        }
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine(" Musteriler siliniyor.. ");
+            Console.WriteLine(" Musterİ Id : "+customer.Id + " Musteri adi : "+customer.Name+" Musteri Soyadi : "+customer.Surname);
+            Console.WriteLine("-----------------------");
+        }
+        public void ListCustomer(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(" Musteriler Listeleniyor , Lütfen Bekleyiniz ");
+                Console.WriteLine(" Musteri Id : " + customer.Id +  " Musteri Adı : " + customer.Name + " Musteri Soyadı : " + customer.Surname);
+                Console.WriteLine(" Musteriler Listelendi ");
+            }
+            Console.WriteLine("-----------------------");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Semih";
+            customer1.Surname = "Çankaya";
+            customer1.Age = 25;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Mehmet";
+            customer2.Surname = "Önemli";
+            customer2.Age = 23;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Elif nur";
+            customer3.Surname = "Önemli";
+            customer3.Age = 18;
+
+            Customer customer4 = new Customer();
+            customer4.Id = 4;
+            customer4.Name = "Selim";
+            customer4.Surname = "Kaya";
+            customer4.Age = 22; 
+            
+            Customer customer5 = new Customer();
+            customer5.Id = 5;
+            customer5.Name = "Kerem";
+            customer5.Surname = "Say";
+            customer5.Age = 19;
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4, customer5 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.AddCustomer(customer1);
+            musteriManager.DeleteCustomer(customer2);
+            musteriManager.ListCustomer(customers);
+
+        }
+    }
+}
